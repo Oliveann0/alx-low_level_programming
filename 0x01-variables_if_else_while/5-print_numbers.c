cd8c13fd92dddd2eb7ec75996d4c8d7f8af84d71
Oliveann0 <jamesblessinganie1@gmail.com>
2022-09-11 11:07:28 -0700

    Added C file 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point.
+ * Description: Program to print members of base 10.
+ * Return: 0 Success.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%i", i);
+	}
+	putchar('\n');
+	return (0);
+}
