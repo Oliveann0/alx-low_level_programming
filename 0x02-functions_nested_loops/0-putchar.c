e5ead07f9d3d8734ae0eedd35a8fd0fb9b15793c
Oliveann0 <jamesblessinganie1@gmail.com>
2022-09-13 18:16:48 -0700

    Added 0-putchar.c file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *
+ *  * main - Print "_putchar" followed by a new line.
+ *
+ *   * Description: You are not allowed to include standard libraries.
+ *
+ *    * Return: 0
+ *
+ *     */
+
+int main(void)
+{
+	char chars[] = "_putchar";
+	int i = 0;
+
+	while (chars[i] != '\0')
+	{
+		_putchar(chars[i]);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}
