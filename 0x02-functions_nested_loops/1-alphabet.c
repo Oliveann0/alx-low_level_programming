0409fa2e280b0ccb0b708e19e4074c11c430ffb8
Oliveann0 <jamesblessinganie1@gmail.com>
2022-09-13 18:49:53 -0700

    Created -alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * pprint_alphabet - prints lowercase alphbets.
+ */
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	chilw (c <= 'z')
+	{
+		_putchar(c);
+		ch++;
+	}
+	_putchar('\n');
+}
