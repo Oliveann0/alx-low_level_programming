4b0c514c4922ab33ee1b9f60376ebe306b2d8e2e
Samuel Ibanga <ibangajnr27@gmail.coom>
2022-09-23 00:57:00 +0100

    Created C source code file 6-cap_String.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * cap_string - Function that capitalizes all words of a string.
+ * @str: String to capitalize.
+ * Return: Capitalized string.
+ */
+char *cap_string(char *s)
+{
+	int i = 0, j;
+	char a[] = " \t\n,;.!?\"(){}";
+
+	while (*(s + i))
+	{
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
+		{
+			if (i == 0)
+				*(s + i) -= 'a' - 'A';
+			else
+			{
+				for (j = 0; j <= 12; j++)
+				{
+					if (a[j] == *(s + i - 1))
+						*(s + i) -= 'a' - 'A';
+				}
+			}
+		}
+		i++;
+	}
+	return (s);
+}
