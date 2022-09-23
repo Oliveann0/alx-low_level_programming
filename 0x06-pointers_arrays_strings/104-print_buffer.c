5665ba3af8c3e4de37c523c81c49e2960c80d836
Oliveann0 <jamesblessingannie1@gmail.com>
2022-09-23 01:41:25 +0100

    Created 104-print_buffer.c

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_buffer - Fnction that prints buffer.
+ * @b: Bytes of buffer.
+ * @size Size of buffer.
+ */
+void print_buffer(char *b, int size)
+{
+	int i;
+
+	for (i = 0; i <= (size - 1) / 10 && size; i++)
+	{
+		printf("%08x: ", i * 10);
+		if (i < size / 10)
+		{
+			print_line(b, 9, i);
+		}
+		else
+		{
+			print_line(b, size % 10 - 1, i);
+		}
+		putchar('\n');
+	}
+	if (size == 0)
+		putchar('\n');
+}
