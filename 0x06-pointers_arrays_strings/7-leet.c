3fcd1c204e6850a8b89ae182cbf99634e3a889c0
Samuel Ibanga <ibangajnr27@gmail.coom>
2022-09-23 01:02:44 +0100

    Created C source code file 7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * leet - FUnction that encodes a string into 1337.
+ * @str: String to encode.
+ * Return: Encoded string.
+ */
+char *leet(char *s)
+{
+	int i, j;
+	char a[] = "aAeEoOtTlL";
+	char b[] = "4433007711";
+
+	for (i = 0; *(s + i); i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if (a[j] == *(s + i))
+				*(s + i) = b[j];
+		}
+	}
+	return (s);
+}
